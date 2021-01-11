c399da76322aeb9f6988350c5186176cb93a6532
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-01-11 16:51:56 -0500

    BMDB Movie, Actor, Credit

--- src/app/feature/credit/credit-list/credit-list.component.ts
+++ src/app/feature/credit/credit-list/credit-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Credit } from '../../../model/credit.class';
+import { CreditService } from '../../../service/credit.service';
 
 @Component({
   selector: 'app-credit-list',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credit-list.component.css']
 })
 export class CreditListComponent implements OnInit {
+  title = 'Credit'
+  credits = Credit[] = [];
 
-  constructor() { }
+  constructor(private creditSvc: CreditService) { }
 
   ngOnInit(): void {
+    //populate list of credits
+    this.creditSvc.getAll().subscribe(
+      resp => {
+        this.credits = resp as Credit[];
+        console.log('Credits', this.credits);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+
   }
 
 }
--- src/app/service/credit.service.ts
+++ src/app/service/credit.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient} from 'angular/common/http';
+import { Observable } from 'rxjs';
+import { Credit } from '../model/credit.class';
+
+const URL = 'http://localhost:8080/credits/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreditService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+    getAll(): Observable<Credit[]> {
+      console.log("creditSvc.getAll()..." + URL);
+      return this.http.get(URL+'/') as Observable<Credit[]>;
+    }
+
 }
