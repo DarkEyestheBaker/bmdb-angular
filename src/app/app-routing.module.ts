05dd9d11ccb75425db47a1455afb77a9111f80dc
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-01-15 16:49:25 -0500

    Actor Detail, Edit & Delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, ResolvedReflectiveFactory } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NoComponentDefinedComponent } from './core/no-component-defined/no-component-defined.component';
+
 import { ActorCreateComponent } from './feature/actor/actor-create/actor-create.component';
 import { ActorEditComponent } from './feature/actor/actor-edit/actor-edit.component';
 import { ActorDetailComponent } from './feature/actor/actor-detail/actor-detail.component';
 import { ActorListComponent } from './feature/actor/actor-list/actor-list.component';
+
 import { MovieCreateComponent } from './feature/movie/movie-create/movie-create.component';
 import { MovieDetailComponent } from './feature/movie/movie-detail/movie-detail.component';
 import { MovieEditComponent } from './feature/movie/movie-edit/movie-edit.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,38 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MovieListComponent } from './feature/movie/movie-list/movie-list.component';
-import {HttpClientModule} from '@angular/common/http';
-import { ActorListComponent } from './feature/actor/actor-list/actor-list.component';
+import { HttpClientModule } from '@angular/common/http';
 import { CreditListComponent } from './feature/credit/credit-list/credit-list.component';
-import { MovieCreateComponent } from './feature/movie/movie-create/movie-create.component';
-import { ActorCreateComponent } from './feature/actor/actor-create/actor-create.component';
 import { NoComponentDefinedComponent } from './core/no-component-defined/no-component-defined.component';
 import { MenuComponent } from './core/menu/menu.component';
+
+import { MovieListComponent } from './feature/movie/movie-list/movie-list.component';
+import { MovieCreateComponent } from './feature/movie/movie-create/movie-create.component';
 import { MovieDetailComponent } from './feature/movie/movie-detail/movie-detail.component';
 import { MovieEditComponent } from './feature/movie/movie-edit/movie-edit.component';
+
+import { ActorListComponent } from './feature/actor/actor-list/actor-list.component';
+import { ActorCreateComponent } from './feature/actor/actor-create/actor-create.component';
 import { ActorDetailComponent } from './feature/actor/actor-detail/actor-detail.component';
 import { ActorEditComponent } from './feature/actor/actor-edit/actor-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    
     MovieListComponent,
-    ActorListComponent,
-    CreditListComponent,
     MovieCreateComponent,
-    ActorCreateComponent,
-    NoComponentDefinedComponent,
-    MenuComponent,
     MovieDetailComponent,
     MovieEditComponent,
+
+    ActorListComponent,
+    ActorCreateComponent,
     ActorDetailComponent,
-    ActorEditComponent
+    ActorEditComponent,
+
+    CreditListComponent,
+    NoComponentDefinedComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
