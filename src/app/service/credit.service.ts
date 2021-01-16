8f19c36525d90623adc6bdfec7a511e71be64799
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-01-16 13:51:08 -0500

    Credit: List, Edit, Create, Delete

--- src/app/service/credit.service.ts
+++ src/app/service/credit.service.ts
@@ -12,26 +12,29 @@ export class CreditService {
 
   constructor(private http: HttpClient) { }
 
-  //service functions
-  //getAll credits
+    //service functions
+
+    //getAll credits
     getAll(): Observable<Credit[]> {
-      console.log("creditSvc.getAll()..." + URL);
       return this.http.get(URL+'/') as Observable<Credit[]>;
     }
+
+      //get credit by ID
+      getById(id): Observable<Credit> {
+      return this.http.get(URL+'/' + id) as Observable<Credit>;
+      }
+
         //create/add credit
-    create(credit: Credit): Observable<Credit> {
-    return this.http.post(URL+'/', credit) as Observable<Credit>;
-  }
-        //get credit by ID
-    getById(id): Observable<Credit> {
-    return this.http.get(URL+'/' + id) as Observable<Credit>;
-  }
+      create(credit: Credit): Observable<Credit> {
+      return this.http.post(URL+'/', credit) as Observable<Credit>;
+      }
+
         //update credit
-    update(credit: Credit): Observable<Credit> {
-    return this.http.put(URL+'/', credit) as Observable<Credit>;
-  }
+      update(credit: Credit): Observable<Credit> {
+      return this.http.put(URL+'/', credit) as Observable<Credit>;
+      }
         //delete credit by ID
-    delete(id): Observable<Credit> {
-  return this.http.delete(URL+'/' + id) as Observable<Credit>;
+      delete(id): Observable<Credit> {
+      return this.http.delete(URL+'/' + id) as Observable<Credit>;
 }
 }
