94c03ea62ebecea78034034b9da1698cca9f14c3
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-01-16 12:59:05 -0500

    Credit:  List, Edit, & Delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,24 +12,31 @@ import { MovieDetailComponent } from './feature/movie/movie-detail/movie-detail.
 import { MovieEditComponent } from './feature/movie/movie-edit/movie-edit.component';
 import { MovieListComponent } from './feature/movie/movie-list/movie-list.component';
 
+import { CreditCreateComponent } from './feature/credit/credit-create/credit-create.component';
+import { CreditDetailComponent } from './feature/credit/credit-detail/credit-detail.component';
+import { CreditEditComponent } from './feature/credit/credit-edit/credit-edit.component';
+import { CreditListComponent } from './feature/credit/credit-list/credit-list.component';
+
+
 const routes: Routes = [
   { path: '', redirectTo: 'movie-list', pathMatch: 'full' },
   { path: 'movie-list', component: MovieListComponent},
   { path: 'movie-create', component: MovieCreateComponent},
   { path: 'movie-detail/:id', component: MovieDetailComponent},
   { path: 'movie-edit/:id', component: MovieEditComponent},
-,
+
   { path: 'actor-list', component: ActorListComponent},
   { path: 'actor-create', component: ActorCreateComponent},
   { path: 'actor-detail/:id', component: ActorDetailComponent},
   { path: 'actor-edit/:id', component: ActorEditComponent},
-  { path: '**', component: NoComponentDefinedComponent},
 
-  { path: 'credit-list', component: ActorListComponent},
-  { path: 'credit-create', component: ActorCreateComponent},
-  { path: 'credit-detail/:id', component: ActorDetailComponent},
-  { path: 'credit-edit/:id', component: ActorEditComponent}
 
+  { path: 'credit-list', component: CreditListComponent},
+  { path: 'credit-create', component: CreditCreateComponent},
+  { path: 'credit-detail/:id', component: CreditDetailComponent},
+  { path: 'credit-edit/:id', component: CreditEditComponent},
+
+  { path: '**', component: NoComponentDefinedComponent}
 ];
 
 
--- src/app/model/movie.class.ts
+++ src/app/model/movie.class.ts
@@ -6,12 +6,15 @@ export class Movie {
     director: string;
 
     // can only construct ONE constructor in javascript
-    constructor(id: number = 0, title: string = '', year: number = 0, rating: string = '', director: string = '')
+    constructor(id: number = 0, title: string = '', 
+                year: number = 0, 
+                rating: string = '', 
+                director: string = '')
      {
          this.id = id;
          this.title = title;
          this.year = year;
          this.rating = rating,
          this.director = director;
-}
+    }
 }
